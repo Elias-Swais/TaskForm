2d1cdb5ddd4c3de1529b9a4a312faeba3a034a8d
Elias Al-Swais <elias.al-swais@pwc.com>
2023-03-14 08:07:58 +0300

    Added another html page that has a form that creates an unordered list depending on the user input

---
+++ Script/script.js
@@ -0,0 +1,49 @@
+const muesumForm = document.querySelector('#form1');
+const usernameInput = document.querySelector('#username');
+const favouritePainting = document.querySelector('#favourite');
+const paintingsList = document.querySelector('#userPaintings');
+
+muesumForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e){
+e.preventDefault();
+    if(usernameInput.value == '' || favouritePainting.value == ''){
+        alert('Please Enter the missing fields');
+    }
+    else{
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode(`Name: ${usernameInput.value}, Painting:${favouritePainting.value}`));
+        paintingsList.appendChild(li);
+        
+        usernameInput.value = '';
+        favouritePainting.value = '';
+    }
+}
+
+function changeColor(){
+    var color = prompt("What color would you like to change the paragraph to?")
+    var test =document.getElementsByClassName("middleParagraph")
+    for(var i = 0; i<test.length; i++){
+        test[i].style.color = color;
+    }
+}
+
+function changeDirection(){
+    var direction = prompt("What direction do you want the images to be?")
+    document.getElementById("imagegallery").style.flexDirection = direction;
+    
+}
+
+function removeText(){
+    let text;
+    if(confirm("Would you like to remove the paragraph?") == true){
+        text = "";
+    }
+        else{
+        text = document.getElementById("middleText").outerHTML;
+        alert("No changes have been done to the paragraph")
+    }
+    document.getElementById("middleText").innerHTML = text;
+    
+}
+
